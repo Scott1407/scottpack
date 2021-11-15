6fcb62548e75fbc1e50b9b69e8f95ed5746fdd74
Scott Lichtenstein <scott.lichtenstein1407@gmail.com>
2021-11-15 14:34:15 +0100

    Initial Push

---
+++ scottpack/lib.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import string
+
+def try_me():
+    alpha = string.ascii_lowercase
+    return alpha
+
+if __name__ == "__main__":
+    alphabet = try_me()
+    print(alphabet)
---
+++ tests/lib_test.py
@@ -0,0 +1,4 @@
+from scottpack.lib import try_me
+
+def test_alpha_length():
+    assert len(try_me()) == 26
